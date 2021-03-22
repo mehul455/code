ab127dddd8ef17e4bae84d7ffa9b5bc4402708b3
mehul455 <79016446+mehul455@users.noreply.github.com>
2021-03-22 17:18:23 +0530

    Add files via upload

---
+++ Dynamic website/about.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://fonts.goooleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+<body>
+<!-- navbar -->
+<?php require 'partials/_nav.php'?>
+
+
+   
+  
+  <section   class="my-5" style="margin-top:30px;">
+<div class="py-5">
+    <h2 class="text-center" style="color:black;"><b>About Us</b></h2>
+</div>
+<div class="container-fluid">
+   <div class="row">
+      <div class="col-lg-6 col-md-6 col-12">
+      <img src="images/1212.jpg" class="img-fluid aboutimg">
+  </div>
+  <div class="col-lg-6 col-md-6 col-12">
+  <h2 class="display-4" style="color:black; font-size:50px;"><b>More about M.K.E.S and Pre-Primary</b></h2>
+      <p class="py-3"style="color:black;"></p>
+      <a href="check.php" class="btn btn-success">check More</a>
+   </div>   
+</div>  
+</div>
+</section>
+
+<footer >
+   <div style="height:150px; margin-top:30px; width:100%; background-color: black; text-align:center;">
+      <div style="color:white; padding:5%;">
+         &copy; <b>2021 | Created & designed by</b> <a href="#" style="color: #48dbfb">WebGohil</a>
+      </div>
+      <div  class="sm">
+        <a href="#" style="color: #48dbfb" class="fab fa-facebook-f"></a>
+        <a href="#" style="color: #48dbfb" class="fab fa-twitter"></a>
+        <a href="#"  style="color: #48dbfb"class="fab fa-instagram"></a>
+        <a href="#"  style="color: #48dbfb"class="fab fa-linkedin-in"></a>
+        <a href="#"  style="color: #48dbfb"class="fab fa-youtube"></a>
+      </div>
+   </div>
+</footer>
+</body>
+</html>
---
+++ Dynamic website/check.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+#b{
+    border:7px solid #48dbfb;
+    padding:20px;
+}
+
+#r{
+    margin-left:20%;
+}
+
+    </style>
+</head>
+<body>
+    <p id="b"  style="font-size:25px; background-color: #353b48; color:white; ">
+                 <br>The school provides students a broad-based exposure to environmental issues and sanitizes them through various international dimension activities. This motivates students to innovate for progress and change, to respect values and to become sensitive and compassionate human beings. The School has been a pioneer in innovative educational practices. Various activities such as Inter-school competitions, Annual Sports, Annual cultural programme, Medical-check up, Intra-Sports Meet, Educational trips and picnics, Workshops and Seminars, Public Awareness Programmes, Prize Distribution Function etc. are conducted throughtout the year.<br><br><br><br>
+                 The school has been the recipient of trophies at inter-school competition, inter-school exams, among them the following achievements are also added to its list:<br><br><br>
+    1.The Best Headmistress Mayor Award - Mrs. Kalpana J. Dogra<br>
+    2.The British Council’s International School Award(2012-2015).<br>
+    3.The British Council’s Global Teacher Accreditation Award - Mrs. Kalpana J. Dogra<br><br>
+    <img src="images/school/img1.jpg" id="r" alt="Chicago"  width="500" height="300"><br>
+
+The TEAM behind all endeavors is the staff which is capable and dedicated to its work and believes, “together each achieves more”
+
+
+
+
+    
+    
+    
+    </p>
+</body>
+</html>
+
+
